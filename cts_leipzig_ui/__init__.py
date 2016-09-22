3012fab176afee79e9a1d7113c42367908092b81
Thibault Clerice <leponteineptique@gmail.com>
2016-09-22 18:08:06 +0200

    Forgot to rename

--- cts_leipzig_ui/__init__.py
+++ cts_leipzig_ui/__init__.py
@@ -4,7 +4,7 @@ from flask_nemo.plugin import PluginPrototype
 from pkg_resources import resource_filename
 
 
-class AperireUI(PluginPrototype):
+class CTSLeipzigUI(PluginPrototype):
     """
         The Breadcrumb plugin is enabled by default in Nemo.
         It can be overwritten or removed. It simply adds a breadcrumb
